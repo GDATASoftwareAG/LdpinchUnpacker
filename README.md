0e275bf0a80876b830ec72bf315d42cd3cb6fee4
Stefan Hausotte <stefan.hausotte@gdata.de>
2020-01-02 08:16:05 +0100

    Add Rust version of the unpacker

--- README.md
+++ README.md
@@ -1,8 +1,51 @@
 # Ldpinch Unpacker
 
-Unpacker example for the Ldpinch malware. 
+Unpacker example for the Ldpinch malware.
 
 Tested with the Ldpinch sample with the SHA256: cc65200e7c748e095f65a8d22ecf8618257cc1b2163e1f9df407a0a47ae17b79
 
 For more information see the blog post about unpacking Ldpinch: [Unpacking 101: Writing a static Unpacker for Ldpinch](https://www.gdatasoftware.com/blog/2019/01/31413-unpack-lpdinch-malware)
 
+## Usage
+
+You can find a version of the unpacker written in *C* and one in *Rust* in the repository.
+
+### Rust Version
+
+```bash
+> ./ldpinch_unpacker
+LdpInch Unpacker 1.0.0
+Unpack LdpInch malware.
+
+USAGE:
+    ldpinch_unpacker [OPTIONS] -i <input>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -i <input>         LdpInch file to unpack.
+    -o <output>        Unpacked output file. [default: unpacked.bin]
+```
+
+### C Version
+
+```bash
+> ./ldpinch_unpacker
+LDPINCH Unpacker\nUsage: ldpinch_unpacker [input] [output]
+```
+
+Like the *Rust* version, the *C* version has a default `output` value set to `unpacked.bin`.
+
+## Build
+
+The *Rust* version can be easily build with:
+
+```bash
+cargo build --release
+```
+
+**Attention**: It's important to build for release and not debug, as the unpacker uses an byte over- and underflow, with will be catched by *Rust* in *Debug* mode.
+
+To build the *C* version open the project in *Visual Studio* and build.
---
+++ rust/src/main.rs
@@ -0,0 +1,62 @@
+extern crate clap;
+use clap::{App, Arg};
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+
+fn main() -> io::Result<()> {
+    let matches = App::new("LdpInch Unpacker")
+        .version("1.0.0")
+        .about("Unpack LdpInch malware.")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .help("LdpInch file to unpack.")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .help("Unpacked output file.")
+                .default_value("unpacked.bin"),
+        )
+        .get_matches();
+
+    let input = matches.value_of("input").unwrap();
+    let output = matches.value_of("output").unwrap();
+
+    let mut buffer = read_binary(input)?;
+    unpack(0x480, 0x1773, &mut buffer);
+    write_binary(output, &mut buffer)?;
+
+    Ok(())
+}
+
+fn read_binary(file: &str) -> io::Result<Vec<u8>> {
+    let mut f = File::open(file)?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+    return Ok(buffer);
+}
+
+fn write_binary(file: &str, buffer: &Vec<u8>) -> io::Result<()> {
+    let mut f = File::create(file)?;
+    f.write_all(buffer)?;
+    Ok(())
+}
+
+fn unpack(start: usize, end: usize, buffer: &mut Vec<u8>) -> () {
+    for i in start..end + 1 {
+        buffer[i] ^= 0x89;
+    }
+
+    buffer[start] ^= 0x9f;
+    let mut key = 0x54;
+    for i in start + 1..end + 1 {
+        buffer[i] ^= key;
+        key += 0x12; // Leads to overflow
+        key ^= 0x68;
+        key -= 0x04; // Lead to undercflow
+    }
+}
